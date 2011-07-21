195884ca848e1a1e57500ab6ed4b1890c8468ee1
Anirudh Joshi <github@anirudhjoshi.com>
2011-07-21 23:10:20 +0000

    Updated pathogen as well

--- update_all.sh
+++ update_all.sh
@@ -2,6 +2,18 @@
 
 return_dir=`pwd`
 
+cd ~/.vim/autoload/
+wget -qN https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.    vim
+
+if grep -qEe '(filetype plugin off|call pathogen#infect\(\)|filetype indent     on)' ~/.vimrc
+then
+        echo "Pathogen already in .vimrc. Passing..." && echo
+else
+        sed -i '1i filetype plugin off\ncall pathogen#infect()\nfiletype ind    ent on' ~/.vimrc
+        echo "Prepended pathogen init to .vimrc." && echo
+fi
+
+
 cd ~/.vim/bundle
 
 for i in `ls`
