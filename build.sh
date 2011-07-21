3a5591ecd52df827506eb1e2af4974d260b30b37
Anirudh Joshi <github@anirudhjoshi.com>
2011-07-21 23:01:49 +0000

    Added update all script

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-PATHOGEN_PACKAGE_NAME="vim-markdown"
-PATHOGEN_PACKAGE_SHORT_DESCRIPTION="It handles syntax, indenting, and compiling."
-PATHOGEN_PACKAGE_NAME_URL="http://www.vim.org/scripts/script.php?script_id=2332"
+PATHOGEN_PACKAGE_NAME="vim-haml"
+PATHOGEN_PACKAGE_SHORT_DESCRIPTION="Vim runtime files for Haml, Sass, and SCSS."
+PATHOGEN_PACKAGE_NAME_URL="http://www.vim.org/scripts/script.php?script_id=1433"
 YOUR_GITHUB_USERNAME="anirudhjoshi"
 
 return_dir=`pwd`
